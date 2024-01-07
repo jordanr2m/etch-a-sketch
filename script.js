2b9d930e786e97495e6249d062f044a0814146e2
Jordan J <jordanr2m@gmail.com>
2024-01-07 11:10:45 -0800

    Added function to clear canvas.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakeButton = document.querySelector('.shake');
 // Variable for amount for line to move
-const MOVE_AMOUNT = 10;
+const MOVE_AMOUNT = 15;
 
 // Choose a random starting point - take width & height, and generate a random number between 0 and the width & 0 and the height
 // Make a variable called width & height from the same properties on canvas
@@ -14,10 +14,13 @@ const { width, height } = canvas;
 let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
 
-// Set up our canvas for drawing
+// Set up our canvas for drawing & place initial point
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = MOVE_AMOUNT;
+let hue = 0; // set h in hsl to 0
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`; // change snake color
+
 // Put our first point somewhere
 ctx.beginPath(); // starts the drawing
 ctx.moveTo(x, y); // Put it somewhere
@@ -28,6 +31,9 @@ ctx.stroke(); // draws line between where you started & where you stopped
 // Write a draw function. Pass destructured object that creates key variable
 function draw({ key }) {
     // console.log(key);
+    hue += 1; // increment hue
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`; // update stroke style each time
+
     ctx.beginPath(); // start path
     ctx.moveTo(x, y); // move to correct spot
     // move x and y values depending on what the user did
@@ -64,7 +70,18 @@ function handleKey(e) {
 }
 
 // Clear or shake function
+function clearCanvas() {
+    canvas.classList.add('shake'); // Add shake first
+    ctx.clearRect(0, 0, width, height); // clears canvas
+    canvas.addEventListener('animationend', function() {
+        console.log('Done the shake');
+        canvas.classList.remove('shake');
+    }, 
+    { once: true }
+    );
+}
 
 
 // Listen for arrow keys on window object
 window.addEventListener('keydown', handleKey);
+shakeButton.addEventListener('click', clearCanvas);
